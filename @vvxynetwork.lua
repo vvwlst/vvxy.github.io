cd31a406df7c0685cdd7398ac05e5659a3880ba1
vvxy <apacoba386@gmail.com>
2026-04-05 10:32:41 +0700

    Add files via upload

---
+++ @vvxynetwork.lua
@@ -0,0 +1,7 @@
+script_name("Chat Custom ZxG")
+script_author("hearttt")
+
+function main()
+    repeat wait(0) until isSampAvailable()
+    sampAddChatMessage("[5/6, 18.58] m: jawab aku juga suka sama dia", 0xFFFF0000)
+end
